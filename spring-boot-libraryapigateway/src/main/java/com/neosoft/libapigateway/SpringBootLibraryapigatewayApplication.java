642d195e33f3b54f313de5d570ebddd8cac61abe
sateesh <sateeshsatee30@gmail.com>
2022-08-10 18:29:13 +0530

    c

---
+++ spring-boot-libraryapigateway/src/main/java/com/neosoft/libapigateway/SpringBootLibraryapigatewayApplication.java
@@ -0,0 +1,15 @@
+package com.neosoft.libapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class SpringBootLibraryapigatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLibraryapigatewayApplication.class, args);
+	}
+
+}
